b9f9fd3acd9ff85013f045d2c7c873d241afb87e
kritirikhi <kritirikhi1999@gmail.com>
2020-06-27 12:58:19 +0530

    model changed

--- blog/migrations/0012_auto_20200623_1815.py
+++ blog/migrations/0012_auto_20200623_1815.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='postview',
             name='viewsno',
-            field=models.AutoField(default=0, primary_key=True, serialize=False),
+            field=models.AutoField(primary_key=True, serialize=False),
             preserve_default=False,
         ),
         migrations.AlterField(
